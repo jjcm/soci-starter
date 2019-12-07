4220f0eddbbe2d3db46bb364f3b6e3fac7b884d1
Jake Miller <j@jjcm.org>
2019-12-07 15:39:27 +1100

    added comments about tag definitions

--- components/example-components.js
+++ components/example-components.js
@@ -1,2 +1,6 @@
+// This imports classes and defines them as custom elements. This way we can import the entire component system by just
+// importing this file. Theoretically you could call window.customElements.define inside of each of the component files 
+// themselves, but this is seen as a bad practice as it means you may have namespace collisions with tags that have the
+// same name. Personally I feel like that's a weak argument, but it's what the community thinks.
 import ExampleComponent from "./example-component.js"
 window.customElements.define('example-component', ExampleComponent)
